d94e36b8734667fb4ab9b3fd007b9304348c762d
Anton Zhilyaev <anto.zhil.anto.zhil@gmail.com>
2021-02-06 23:17:02 +0300

    added login and registr

--- kofe/models.py
+++ kofe/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from datetime import datetime, timedelta
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from kofeFast import settings
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.db import models
--- kofe/views.py
+++ kofe/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 def index_page(request):
-    context = {}
+    form = None
+
+    if request.method == 'POST':
+        if request.POST.get('action_type') == 'authen':
+            password = request.POST.get('password1')
+            username = request.POST.get('username')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+    context = {
+        'form': form if form else UserCreationForm(),
+    }
     return render(request, 'pages/index.html', context)
