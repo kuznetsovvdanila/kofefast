f18faf84ebff0c3fd5f1e854b236b1cbd4a288ee
Anton Zhilyaev <anto.zhil.anto.zhil@gmail.com>
2021-05-30 06:55:23 +0300

    pylint, i guess..

--- kofeFast/settings.py
+++ kofeFast/settings.py
@@ -135,7 +135,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
