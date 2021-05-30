576f348d847475c3f8ff4b260ae07023189ba8ca
Anton Zhilyaev <anto.zhil.anto.zhil@gmail.com>
2021-05-30 12:33:26 +0300

    some sort of 'changes'

--- kofe/tests.py
+++ kofe/tests.py
@@ -1,4 +1,27 @@
 """Описание тестов для проверки работоспособности приложения"""
-# from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# пустота...
+from kofeFast.settings import AUTH_USER_MODEL
+
+
+class IndexPageTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.response = self.client.get(reverse('index'))
+
+    def test_index_response(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+class PersonalAreaPageTestCase(TestCase):
+    fixtures = ['test_database.json']
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = AUTH_USER_MODEL.objects.get(email='1@gmail.com')
+        self.client.force_login(self.user)
+        self.response = self.client.get(reverse('personal_area'))
+
+    def test_index_response(self):
+        self.assertEqual(self.response.status_code, 200)
