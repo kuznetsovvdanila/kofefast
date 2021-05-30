72e7a2183ef6de51b6999883ac43e731b6845b18
Anton Zhilyaev <anto.zhil.anto.zhil@gmail.com>
2021-05-30 12:52:55 +0300

    pep8/pylint changes

--- kofe/tests.py
+++ kofe/tests.py
@@ -6,22 +6,28 @@ from kofeFast.settings import AUTH_USER_MODEL
 
 
 class IndexPageTestCase(TestCase):
+    """Тесты view 'index.html' """
     def setUp(self) -> None:
+        """Сетап для тестов view 'index.html' """
         self.client = Client()
         self.response = self.client.get(reverse('index'))
 
     def test_index_response(self):
+        """Тест на ответ страницы"""
         self.assertEqual(self.response.status_code, 200)
 
 
 class PersonalAreaPageTestCase(TestCase):
+    """Тесты view 'personal_area.html' """
     fixtures = ['test_database.json']
 
     def setUp(self) -> None:
+        """Сетап для тестов view 'personal_area.html' """
         self.client = Client()
         self.user = AUTH_USER_MODEL.objects.get(email='1@gmail.com')
         self.client.force_login(self.user)
         self.response = self.client.get(reverse('personal_area'))
 
     def test_index_response(self):
+        """Тест на ответ страницы"""
         self.assertEqual(self.response.status_code, 200)
